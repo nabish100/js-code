851e4b6aaed88eaf7e1b89824c8edbb7e0d627cf
NABISH <nabila.muhib100@gmail.com>
2024-11-14 23:09:00 +0430

    Your commit message

--- app.js
+++ app.js
@@ -3,9 +3,25 @@ let num2 = 90;
 let result = num1 + num2;
 console.log(result);
 
+// = >> assignment (x=y  the valuye of y goes to the x)
 
-function add (a,b)
-{
- return a+b;
-}
-console.log(add(10,90));
+let x=1;
+x=x+1;
+x=x+5;
+console.log(x);
+
+// Array  >> a simple way to define mutiple variables
+let name = ["Nabish","dawood","wahid",10,"Akif"] ; // syntax 
+
+console.log(name[0])
+console.log(name[1])
+console.log(name[2])
+console.log(name[4])
+
+let fruit= ["apple",10,"10","mango"];
+
+console.log(fruit[3]);
+// we made a github account >> like instgram >> global repo
+// we made git account >> local repo
+// we made SSH key 
+// git clone , git init , git add . , git remote ------ , git push 
